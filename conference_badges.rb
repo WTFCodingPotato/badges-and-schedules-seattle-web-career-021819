ee548692df6660410c3e0082a727af110a5de3ae
Joshua M Loran <joshua.loran@gmail.com>
2019-01-09 20:35:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  new_list = []
   for attendee in attendees
-    new_list[attendee] = badge_maker(attendee)
+    badge_maker(attendee)
   end
-  new_list
+  attendees
 end
 def assign_rooms
 end
